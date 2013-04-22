b7dfa9c659a50f87314ad64b180a2f13755133df
gulangxiangjie@gmail.com <gulangxiangjie@gmail.com@cc7f1117-c532-2139-29f1-3127c1770406>
2013-04-22 04:53:59 +0000

    增加写日记界面的布局和界面

---
+++ trunk/FoxDiary/src/com/foxchan/foxdiary/view/DiaryWriteView.java
@@ -0,0 +1,21 @@
+package com.foxchan.foxdiary.view;
+
+import com.foxchan.foxdiary.core.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * 写日记的界面
+ * @author foxchan@live.cn
+ * @create 2013-4-22
+ */
+public class DiaryWriteView extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.diary_write);
+	}
+
+}
