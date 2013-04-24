32dc9556db17b8f0075574a105ca999f59098f59
gulangxiangjie <gulangxiangjie@gmail.com>
2013-04-24 02:59:14 +0000

    添加写日记界面的选择图片和添加声音的界面

--- FoxDiary/src/com/foxchan/foxdiary/view/DiaryWriteView.java
+++ FoxDiary/src/com/foxchan/foxdiary/view/DiaryWriteView.java
@@ -15,7 +15,7 @@ public class DiaryWriteView extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.diary_write);
+		setContentView(R.layout.diary_write_voice);
 	}
 
 }
