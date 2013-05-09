52053c194b188dbb9177f9f6f81a51e22eb94458
gulangxiangjie <gulangxiangjie@gmail.com>
2013-05-09 09:05:39 +0000

    增加FileUtils.java，增加对于文件和文件夹的操作工具类

--- FoxUtils/src/com/foxchan/foxutils/FoxTest.java
+++ FoxUtils/src/com/foxchan/foxutils/FoxTest.java
@@ -1,15 +1,30 @@
 package com.foxchan.foxutils;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 
+import com.foxchan.foxutils.tool.FileUtils;
+
 public class FoxTest extends Activity {
+	
+	public static final String TAG = "FoxUtils";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.test);
+		
+		String fileName = "demo.txt";
+		String ext = FileUtils.getExt(fileName);
+		Log.d(TAG, "文件的拓展名是：" + ext);
+		
+		String[] filePaths = new String[]{
+				"study", "books", "notes"
+		};
+		String filePath = FileUtils.buildFileName(filePaths, fileName);
+		Log.d(TAG, "文件的保存路径是：" + filePath);
 	}
 
 	@Override
