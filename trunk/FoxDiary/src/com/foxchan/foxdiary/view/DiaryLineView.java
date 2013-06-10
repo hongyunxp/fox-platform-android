717ae096643d41786e3dd30c953685937fbcbe98
gulangxiangjie@gmail.com <gulangxiangjie@gmail.com@cc7f1117-c532-2139-29f1-3127c1770406>
2013-06-10 18:01:17 +0000

    更新写日记添加声音的界面的布局和设计

--- trunk/FoxDiary/src/com/foxchan/foxdiary/view/DiaryLineView.java
+++ trunk/FoxDiary/src/com/foxchan/foxdiary/view/DiaryLineView.java
@@ -1,10 +1,16 @@
 package com.foxchan.foxdiary.view;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
@@ -16,12 +22,15 @@ import android.widget.TextView;
 import android.widget.Toast;
 import cn.com.lezhixing.foxdb.core.FoxDB;
 import cn.com.lezhixing.foxdb.core.Session;
+import cn.com.lezhixing.foxdb.table.Column;
 
 import com.foxchan.foxdiary.adapter.DiaryLineAdapter;
 import com.foxchan.foxdiary.adapter.DiaryLineAdapter.NodeListener;
 import com.foxchan.foxdiary.core.R;
 import com.foxchan.foxdiary.entity.Diary;
 import com.foxchan.foxdiary.utils.Constants;
+import com.foxchan.foxutils.data.CollectionUtils;
+import com.foxchan.foxutils.data.DateUtils;
 
 /**
  * 日记时间线界面
@@ -36,17 +45,27 @@ public class DiaryLineView extends Activity {
 	private ListView lvDiarys;
 	/** 日记列表的数据适配器 */
 	private DiaryLineAdapter diaryLineAdapter;
+	/** 日期控件 */
+	private TextView tvShowDate;
+	/** 当前显示的日期 */
+	private Date showDate;
+	/** 用户所有的日记的日期列表 */
+	private static List<Date> diaryDates;
 	
 	private FoxDB db;
+	
+	//添加日记的相关部件
 	private TextView tvContent;
 	private ImageView ivPhoto;
 	private LinearLayout llBalloon;
 	private ImageView ivNode;
+	private TextView tvDateTime;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.diary_line);
+		FoxDB.DEBUG = true;
 		//初始化组件和数据
 		db = FoxDB.create(this, Constants.DIARY_DB_NAME, Constants.DIARY_DB_VERSION);
 		initDatas();
@@ -94,8 +113,11 @@ public class DiaryLineView extends Activity {
 		ivPhoto = (ImageView)findViewById(R.id.diary_line_photo);
 		llBalloon = (LinearLayout)findViewById(R.id.diary_line_balloon);
 		ivNode = (ImageView)findViewById(R.id.diary_line_node);
+		tvDateTime = (TextView)findViewById(R.id.diary_line_item_time);
+		tvShowDate = (TextView)findViewById(R.id.diary_line_header_title);
 		
 		ivPhoto.setVisibility(View.GONE);
+		tvDateTime.setVisibility(View.INVISIBLE);
 		tvContent.setText(getString(R.string.diary_line_add));
 		ivNode.setImageResource(R.drawable.icon_plus_small);
 		llBalloon.setOnClickListener(new OnClickListener() {
@@ -104,6 +126,9 @@ public class DiaryLineView extends Activity {
 				toDiaryWriteView();
 			}
 		});
+		//设置显示的日期为当前的日期
+		String showDateStr = DateUtils.formatDate(showDate, "yyyy年MM月dd日");
+		tvShowDate.setText(showDateStr);
 	}
 	
 	/**
@@ -119,7 +144,26 @@ public class DiaryLineView extends Activity {
 	 */
 	private void initDatas() {
 		Session session = db.openSession();
-		diaries = session.list(Diary.class);
+		//初始化日记的日期列表
+		String sql = "SELECT * FROM tb_core_diary WHERE 1=1 GROUP BY substr(createDate,1,11) ORDER BY createDate asc";
+		List<Diary> tempDiaries = session.querySQL(sql, Diary.class);
+		if(!CollectionUtils.isEmpty(tempDiaries)){
+			diaryDates = new ArrayList<Date>();
+			for(Diary d : tempDiaries){
+				diaryDates.add(d.getCreateDate());
+			}
+		}
+		//初始化默认显示的日记
+		showDate = new Date();
+		GregorianCalendar gc = new GregorianCalendar();
+		gc.setTime(showDate);
+		gc.add(GregorianCalendar.DAY_OF_MONTH, 1);
+		String where = "createDate BETWEEN ? and ?";
+		String[] params = new String[]{
+				DateUtils.formatDate(showDate, "yyyy-MM-dd"),
+				DateUtils.formatDate(gc.getTime(), "yyyy-MM-dd")
+		};
+		diaries = session.list(where, params, Diary.class);
 	}
 
 }
