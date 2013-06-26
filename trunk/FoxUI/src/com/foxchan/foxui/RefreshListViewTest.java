f7d11dc3b5e1529b148def0777705d58b1b18dd3
gulangxiangjie@gmail.com <gulangxiangjie@gmail.com@cc7f1117-c532-2139-29f1-3127c1770406>
2013-06-26 05:03:07 +0000

    修正了RefreshListView的一些BUG

--- trunk/FoxUI/src/com/foxchan/foxui/RefreshListViewTest.java
+++ trunk/FoxUI/src/com/foxchan/foxui/RefreshListViewTest.java
@@ -1,13 +1,20 @@
 package com.foxchan.foxui;
 
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.foxchan.foxui.core.R;
 import com.foxchan.foxui.widget.lang.RefreshListView;
@@ -21,19 +28,96 @@ import com.foxchan.foxui.widget.lang.RefreshListView;
 public class RefreshListViewTest extends Activity {
 	
 	private RefreshListView lv;
-	private String[] arrays;
+	private List<String> arrays;
 	private ListViewAdapter adapter;
+	private int index = 0;
+	private int i = 0;
+	private boolean isRefresh = true;
+	
+	private MyHandler handler = new MyHandler(this);
+	static class MyHandler extends Handler{
+		WeakReference<RefreshListViewTest> reference;
+		
+		public MyHandler(RefreshListViewTest activity){
+			this.reference = new WeakReference<RefreshListViewTest>(activity);
+		}
+		
+		@Override
+		public void handleMessage(android.os.Message msg) {
+			RefreshListViewTest activity = reference.get();
+			switch (msg.what) {
+			case 0:
+				activity.lv.refreshingDataComplete();
+				Toast.makeText(activity, "数据刷新成功！！", Toast.LENGTH_SHORT).show();
+				activity.isRefresh = false;
+				activity.i = 0;
+				break;
+			}
+		}
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.refresh_listview);
 		
-		arrays = new String[20];
+		arrays = new ArrayList<String>();
 		for(int i = 0; i < 20; i++){
-			arrays[i] = "第" + i + "项";
+			arrays.add("第" + i + "项");
 		}
 		lv = (RefreshListView)findViewById(R.id.refresh_listview);
+		
+		lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View v, int postion,
+					long id) {
+				Toast.makeText(RefreshListViewTest.this, "哈哈，这是第" + postion + "个选项", Toast.LENGTH_SHORT).show();
+			}
+		});
+		lv.setOnTaskDoingListener(new RefreshListView.OnTaskDoingListener() {
+
+			@Override
+			public void refreshingData(RefreshListView view) {
+				isRefresh = true;
+				new Thread(){
+					public void run(){
+						while(isRefresh){
+							if(i < 1){
+								i++;
+							} else {
+								handler.sendEmptyMessage(0);
+							}
+							try {
+								Thread.sleep(1000);
+							} catch (InterruptedException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						}
+					}
+				}.start();
+			}
+
+			@Override
+			public void loadMoreData(RefreshListView view) {
+				if(index < 3){
+					List<String> moreData = new ArrayList<String>();
+					for(int i = 0; i < 10; i++){
+						moreData.add("新标签" + i);
+					}
+					arrays.addAll(moreData);
+					adapter.notifyDataSetChanged();
+					if(index == 2){
+						view.loadMoreDataComplete();
+					}
+					index++;
+					Log.d("cqm", "数据集的数量是" + arrays.size());
+				}
+			}
+			
+		});
+		
 		adapter = new ListViewAdapter();
 		lv.setAdapter(adapter);
 	}
@@ -42,12 +126,12 @@ public class RefreshListViewTest extends Activity {
 
 		@Override
 		public int getCount() {
-			return arrays.length;
+			return arrays.size();
 		}
 
 		@Override
 		public Object getItem(int pos) {
-			return (String)arrays[pos];
+			return (String)arrays.get(pos);
 		}
 
 		@Override
@@ -66,7 +150,7 @@ public class RefreshListViewTest extends Activity {
 			} else {
 				title = (TextView)convertView.getTag();
 			}
-			title.setText(arrays[pos]);
+			title.setText(arrays.get(pos));
 			return convertView;
 		}
 		
