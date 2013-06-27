74a162ec10232bb27a2dfdf0c61740c6fd84599e
gulangxiangjie@gmail.com <gulangxiangjie@gmail.com@cc7f1117-c532-2139-29f1-3127c1770406>
2013-06-27 19:36:41 +0000

    增加事务处理的功能，经过测试，添加200条数据的前提下，经过事务消耗的时间是9629ms，未使用事务消耗的时间是17622ms

--- trunk/FoxDB/src/cn/com/lezhixing/foxdb/FoxDBTest.java
+++ trunk/FoxDB/src/cn/com/lezhixing/foxdb/FoxDBTest.java
@@ -1,20 +1,23 @@
 package cn.com.lezhixing.foxdb;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.List;
 
 import cn.com.lezhixing.foxdb.core.FoxDB;
 import cn.com.lezhixing.foxdb.core.Session;
+import cn.com.lezhixing.foxdb.core.Transaction;
 import cn.com.lezhixing.foxdb.engine.Pager;
 import cn.com.lezhixing.foxdb.engine.PagerTemplate;
-
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
 
 public class FoxDBTest extends Activity {
+	
+	private Session session;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +25,15 @@ public class FoxDBTest extends Activity {
 		setContentView(R.layout.start);
 		
 		FoxDB.DEBUG = true;
-		FoxDB db = FoxDB.create(this, "fox.db", 10);
-		Session session = db.openSession();
+		FoxDB db = FoxDB.create(this, "fox.db", 2);
+		session = db.getCurrentSession();
+		
+		new Thread(){
+			public void run(){
+				testTransaction(200);
+				testSave(200);
+			}
+		}.start();
 		
 //		User user = session.findObject("id = ?", new Object[]{2}, User.class);
 //		Log.d("FoxDB", "User's name is " + user.getName());
@@ -33,14 +43,14 @@ public class FoxDBTest extends Activity {
 //		Log.d("FoxDB", "Group's name is " + group.getName());
 //		List<User> users = session.listFrom(group, "users", User.class);
 //		Log.d("FoxDB", group.getName() + "中一共有" + users.size() + "名成员。");
-		
+//		
 //		Group group = session.findObject("id=?", new Object[]{"0fe52f22128646009a6588b94dd2c21e"}, Group.class);
 //		session.delete(group);
-		
+//		
 //		Group group = session.findObject("id=?", new Object[]{"a5874cedcab9407392075994c89b9a1d"}, Group.class);
 //		List<User> users = session.listFrom(-1, -1, "weight > ?", new Object[]{16}, null, group, "users", User.class);
 //		Log.d("FoxDB", users.size() + "");
-		
+//		
 //		Pager<User> pager = new Pager<User>(15, 2);
 //		LinkedHashMap<String, String> orderBy = new LinkedHashMap<String, String>();
 //		orderBy.put("createDate", "desc");
@@ -56,33 +66,76 @@ public class FoxDBTest extends Activity {
 //		for(User u : pager.getContent()){
 //			Log.d("FoxDB", u.toString());
 //		}
+//		
+//		Group group = new Group();
+//		group.setName("小组一");
+//		session.save(group);
+//		
+//		for(int i = 0; i < 43; i++){
+//			SchoolCard card = new SchoolCard();
+//			card.setNumber("2220093210620" + i);
+//			session.save(card);
+//			
+//			User user = new User();
+//			user.setCreateDate(new Date());
+//			user.setIsGood(true);
+//			user.setName("FoxDB" + i);
+//			user.setGroup(group);
+//			user.setCard(card);
+//			session.save(user);
+//		}
 		
-		Group group = new Group();
-		group.setName("小组一");
-		session.save(group);
-		
-		for(int i = 0; i < 43; i++){
-			SchoolCard card = new SchoolCard();
-			card.setNumber("2220093210620" + i);
-			session.save(card);
-			
+//		User user = session.get(2, User.class);
+//		System.out.println("2号用户的信息：" + user.getName());
+//		user.setName("我是2号");
+//		user.setWeight(22.0);
+//		user.setIsGood(true);
+//		session.update(user);
+//		user = session.get(user.getId(), User.class);
+//		System.out.println("2号用户新的信息：" + user.getName());
+	}
+
+	/**
+	 * 数据库的事务测试
+	 */
+	private void testTransaction(int count) {
+		Date start = new Date();
+		List<User> users = new ArrayList<User>();
+		Transaction tx = session.beginTransaction();
+		for(int i = 0; i < count; i++){
 			User user = new User();
+			user.setCard(null);
 			user.setCreateDate(new Date());
+			user.setGroup(null);
+			user.setHeight(173.0f);
 			user.setIsGood(true);
-			user.setName("FoxDB" + i);
-			user.setGroup(group);
-			user.setCard(card);
+			user.setName("用户" + i);
+			user.setWeight(20.00);
+			users.add(user);
 			session.save(user);
 		}
-		
-		User user = session.get(2, User.class);
-		System.out.println("2号用户的信息：" + user.getName());
-		user.setName("我是2号");
-		user.setWeight(22.0);
-		user.setIsGood(true);
-		session.update(user);
-		user = session.get(user.getId(), User.class);
-		System.out.println("2号用户新的信息：" + user.getName());
+		tx.commit();
+		Date end = new Date();
+		long time = end.getTime() - start.getTime();
+		System.out.println("测试结束，总共耗时：" + time + "ms");
+	}
+	
+	private void testSave(int count){
+		Date start = new Date();
+		for(int i = 0; i < count; i++){
+			User user = new User();
+			user.setCard(null);
+			user.setCreateDate(new Date());
+			user.setGroup(null);
+			user.setHeight(173.0f);
+			user.setIsGood(true);
+			user.setName("用户" + i);
+			user.setWeight(20.00);
+			session.save(user);
+		}
+		Date end = new Date();
+		long time = end.getTime() - start.getTime();
+		System.out.println("测试结束，总共耗时：" + time + "ms");
 	}
 
 	@Override
