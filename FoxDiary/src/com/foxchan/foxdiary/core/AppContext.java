2d6ffa9d37253590c428631395341b47d2df5b9b
gulangxiangjie <gulangxiangjie@gmail.com>
2013-07-17 17:08:45 +0000

    完成获取用户定位的功能

--- FoxDiary/src/com/foxchan/foxdiary/core/AppContext.java
+++ FoxDiary/src/com/foxchan/foxdiary/core/AppContext.java
@@ -3,6 +3,9 @@ package com.foxchan.foxdiary.core;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.app.Application;
+
+import com.baidu.location.LocationClient;
 import com.foxchan.foxdiary.entity.Diary;
 import com.foxchan.foxutils.data.CollectionUtils;
 
@@ -11,7 +14,7 @@ import com.foxchan.foxutils.data.CollectionUtils;
  * @author foxchan@live.cn
  * @create 2013-4-28
  */
-public class AppContext {
+public class AppContext extends Application {
 	
 	/** 应用程序的标签 */
 	public static final String TAG = "FoxDiary";
@@ -19,6 +22,15 @@ public class AppContext {
 	/** 显示在日记时间轴的日记列表 */
 	public static List<Diary> diariesOnDiaryLineView;
 	
+	/** 定位的对象 */
+	private LocationClient locationClient;
+	
+	@Override
+	public void onCreate() {
+		locationClient = new LocationClient(getApplicationContext());
+		super.onCreate();
+	}
+
 	/**
 	 * 添加日记到日记时间轴的日记集合中
 	 * @param diary	添加的日记
@@ -28,6 +40,14 @@ public class AppContext {
 			diariesOnDiaryLineView = new ArrayList<Diary>();
 		}
 		diariesOnDiaryLineView.add(diary);
+	}
+
+	public LocationClient getLocationClient() {
+		return locationClient;
+	}
+
+	public void setLocationClient(LocationClient locationClient) {
+		this.locationClient = locationClient;
 	}
 
 }
