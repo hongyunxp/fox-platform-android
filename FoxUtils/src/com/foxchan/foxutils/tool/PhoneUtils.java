eb977611daf41312f0c28fc757923bc6b575d82b
gulangxiangjie <gulangxiangjie@gmail.com>
2013-07-17 04:53:31 +0000

    添加获得屏幕宽度的方法

--- FoxUtils/src/com/foxchan/foxutils/tool/PhoneUtils.java
+++ FoxUtils/src/com/foxchan/foxutils/tool/PhoneUtils.java
@@ -2,7 +2,10 @@ package com.foxchan.foxutils.tool;
 
 import java.io.IOException;
 
+import android.app.Activity;
 import android.media.MediaPlayer;
+import android.view.Display;
+import android.view.WindowManager;
 
 /**
  * 该类提供与手机提供的基本信息相关的方法
@@ -27,6 +30,17 @@ public class PhoneUtils {
 		mp.setDataSource(audioPath);
 		mp.prepare();
 		mp.start();
+	}
+	
+	/**
+	 * 获得手机的屏幕宽度
+	 * @param activity
+	 * @return			返回手机屏幕的宽度
+	 */
+	public static int getWindowWidth(Activity activity){
+		WindowManager manager = activity.getWindowManager();
+		Display display = manager.getDefaultDisplay();
+		return display.getWidth();
 	}
 
 }
