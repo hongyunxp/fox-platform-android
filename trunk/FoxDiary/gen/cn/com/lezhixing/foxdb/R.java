38fc861415aa9938ed5249cac1061e17a257f07d
gulangxiangjie@gmail.com <gulangxiangjie@gmail.com@cc7f1117-c532-2139-29f1-3127c1770406>
2013-07-18 03:11:23 +0000

    修复平板电脑在WIFI模式下获取不到定位信息的BUG

--- trunk/FoxDiary/gen/cn/com/lezhixing/foxdb/R.java
+++ trunk/FoxDiary/gen/cn/com/lezhixing/foxdb/R.java
@@ -15,7 +15,7 @@ public final class R {
 		public static final int ic_launcher = 0x7f020044;
 	}
 	public static final class id {
-		public static final int action_settings = 0x7f0a0062;
+		public static final int action_settings = 0x7f0a0063;
 	}
 	public static final class layout {
 		public static final int start = 0x7f03000c;
