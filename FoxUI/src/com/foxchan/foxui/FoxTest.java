718c20c5042b963961584a4e9c02fdc27f5f91ab
gulangxiangjie <gulangxiangjie@gmail.com>
2013-08-06 17:54:30 +0000

    添加具有卡片效果的控件

--- FoxUI/src/com/foxchan/foxui/FoxTest.java
+++ FoxUI/src/com/foxchan/foxui/FoxTest.java
@@ -6,15 +6,14 @@ import android.view.Menu;
 
 import com.foxchan.foxui.core.R;
 import com.foxchan.foxui.widget.lang.AutoChangeLineViewGroup;
+import com.foxchan.foxui.widget.lang.CardsSwitcher;
 
 public class FoxTest extends Activity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.auto_change_line);
-		AutoChangeLineViewGroup viewGroup = (AutoChangeLineViewGroup)findViewById(R.id.box_container);
-		viewGroup.setChildViewMargin(10);
+		testCardsSwitcher();
 	}
 
 	@Override
@@ -22,6 +21,23 @@ public class FoxTest extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.fox_test, menu);
 		return true;
+	}
+	
+	/**
+	 * 测试自动换行的控件
+	 */
+	protected void testAutoChangeLineViewGroup(){
+		setContentView(R.layout.auto_change_line);
+		AutoChangeLineViewGroup viewGroup = (AutoChangeLineViewGroup)findViewById(R.id.box_container);
+		viewGroup.setChildViewMargin(10);
+	}
+	
+	/**
+	 * 测试卡片切换控件
+	 */
+	protected void testCardsSwitcher(){
+		setContentView(R.layout.cards_switcher_test);
+		CardsSwitcher cardsSwitcher = (CardsSwitcher)findViewById(R.id.cards_switcher);
 	}
 
 }
