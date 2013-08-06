808f04e27be8e267c5092123dcf0083dd1da57be
gulangxiangjie <gulangxiangjie@gmail.com>
2013-08-06 13:17:40 +0000

    添加对多分辨率机器的适配，添加日记横屏的UI设计

--- FoxDiary/src/com/foxchan/foxdiary/core/AppStart.java
+++ FoxDiary/src/com/foxchan/foxdiary/core/AppStart.java
@@ -12,8 +12,8 @@ public class AppStart extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.diary_line_item);
-		toDiaryLineView();
+		setContentView(R.layout.diary_landscape);
+//		toDiaryLineView();
 	}
 
 	@Override
